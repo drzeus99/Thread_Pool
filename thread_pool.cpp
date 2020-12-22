1d995f076519cc019e0e2305fcdb67e2dba8db76
drzeus99 <pandakraus@gmail.com>
2020-12-22 10:25:43 -0500

    final thread pool

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,62 +1,59 @@
+//
+//  thread_pool.cpp
+//
+//
+//  Created by Zachary Kraus on 2/18/16.
+//
+//
+
 #include "thread_pool.h"
-#include "thread_helper.h"
 using namespace std;
 
-thread_pool::thread_pool() : max_threads(2)
+thread_pool::thread_pool(unsigned long long init_pool_size, unsigned int thread_count, bool threaded_write)
 {
-    threads.resize(2);
+	storage.resize(init_pool_size);
+	//pool.resize(thread_count);
+	this->thread_count = thread_count;
+    storage.begin_read_write(threaded_write); //this step ensures multi threaded inserting does not cause data races
+	this->threaded_write = threaded_write;
 }
 
-thread_pool::thread_pool(int num) : max_threads(num)
+void thread_pool::insert(shared_ptr<void>&& input, void* (*method) (void*))
 {
-    threads.resize(max_threads);
-}
-void thread_pool::add_job(void *in)
-{
-    jobs.push_back(in);
+	int process_num = num.fetch_add(1);
+	storage.insert(process_num, helper(move(input), method));
 }
 
-void thread_pool::run_jobs(int start, int end)
+void thread_pool::begin()
 {
-    int tasks;
-    //initialize number of tasks to be run
-    if (end == 0)
-        tasks = jobs.size();
-    else  //fix bug here
-    {
-        if (end > jobs.size())
-        {
-            end = jobs.size();
-        }
-        tasks = end;
-    }
-    //initialize starting task
-    if (start<0)
-    {
-        start = 0;
-    }
-    int task = start;
-    //initialize number of threads currently running
-    int thread_num = 0;
-    //execute thread_pool
-    while (task < tasks )
-    {
-        while (thread_num < max_threads)
-        {
-            //cout<<"begining thread "<<task<<" with" <<thread_num<<" active\n";
-            pthread_create(&threads[thread_num], NULL, method, jobs[task]);
-            //cout<<"finished creating thread\n";
-            //cout<<"finised thread "<<task<<" thread num is now "<<thread_num<<endl;
-            thread_num++;
-            task++;
-            if (task >= tasks)
-                break;
-        }
-        //join threads
-        for (int cnt = 0; thread_num > 0; thread_num--, cnt++)
-        {
-            //cout<<"the thread num is "<<cnt<<endl;
-            pthread_join(threads[cnt], NULL);
-        }
-    }
+	storage.end_read_write(threaded_write);
+	storage.begin_read_write(threaded_write);
+	for (unsigned int i = 0; i < thread_count; i++)
+		pool.push_back(thread(&thread_pool::batch_run, this, i, thread_count));
+}
+
+void thread_pool::end()
+{
+	pool_exit = true;
+	for (auto& val: pool)
+		val.join();
+}
+
+void thread_pool::batch_run(int start_index, int thread_num)
+{
+	while (!pool_exit) {
+		for (unsigned int i = start_index; i < storage.hash_size(); i += thread_num) {
+			for (auto& item: storage.batch(i))
+				item.second.method(item.second.in.get());
+			storage.move_write_batch(i);
+		}
+	}
+	//code to guarantee correct exiting
+	for (int j = 0; j < 2; j++) {
+		for (unsigned int i = start_index; i < storage.hash_size(); i += thread_num) {
+			for (auto& item: storage.batch(i))
+				item.second.method(item.second.in.get());
+			storage.move_write_batch(i);
+		}
+	}
 }
--- thread_pool.h
+++ thread_pool.h
@@ -1,23 +1,43 @@
-#ifndef THREAD_POOL_H
-#define THREAD_POOL_H
-#include <vector>
-#include <pthread.h>
+//
+//  thread_pool.h
+//
+//
+//  Created by Zachary Kraus on 2/18/16.
+//
+//
 
-class thread_pool //cannot have mixed class even though its a void*
+#ifndef ____thread_pool__
+#define ____thread_pool__
+
+#include <atomic>
+#include "kmap.h"
+#include <thread>
+#include <vector>
+#include <memory>
+
+//do we need to pause threads when lack of processes exists
+class thread_pool
 {
-    //should I just change this to a templated class as well
 public:
-    thread_pool();
-    thread_pool(int num);
-    void add_job(void *in);
-    void run_jobs(int start=0, int end=0);
+	thread_pool(unsigned long long, unsigned int, bool = true);
+	void insert(std::shared_ptr<void>&&, void* (*method) (void*));
+	void begin();
+	void end();
 private:
-    std::vector<void *> jobs; //Since these pointers are passed to the class
-    //they do not need deleting in the class.
-    //this class is a temporary owner of these pointers.
-    std::vector<pthread_t> threads;
-    int max_threads;
+	std::vector<std::thread> pool;
+	std::atomic<unsigned long long> num {1};
+	bool threaded_write;
+	struct helper
+	{
+		helper() {};
+		helper(std::shared_ptr<void>&& in, void* (*method) (void*)) : method(method) {this->in = move(in);}
+		std::shared_ptr<void> in;
+		void* (*method) (void*);
+	};
+	kmap<unsigned long long, helper> storage;
+	bool pool_exit = false;
+	void batch_run(int, int);
+	unsigned int thread_count;
 };
 
-
-#endif // THREAD_POOL_H
+#endif /* defined(____thread_pool__) */
